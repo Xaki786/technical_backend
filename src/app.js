0a64563d97c93c7d6a177a183883a88da06d3a7c
zakria <zakria.jan@10pearls.com>
2021-12-30 12:03:03 +0500

    db connected

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-
 app.use(cors());
 app.use((req, res, next) => {
   res.json({
@@ -13,6 +13,16 @@ app.get("/", (req, res, next) => {
   res.send("New Hello Deploy");
 });
 const PORT = process.env.PORT || 5000;
+const uri =
+  "mongodb+srv://testUser:KhZhNnwYRPJV3jz@technicaldb.dvxpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the Database");
+  });
 app.listen(PORT, () => {
   console.log("Server has started!");
 });
