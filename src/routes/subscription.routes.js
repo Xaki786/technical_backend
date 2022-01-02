0e6b952cb8a413c3804721dcfcd891e753136a70
zakria <zakria.jan@10pearls.com>
2022-01-02 15:18:28 +0500

    added src/public folder in the repo

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -9,7 +9,7 @@ const { v4: uuidv4 } = require("uuid");
 let path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname + "/src/public/images/");
+    cb(null, "src/public/images/");
   },
   filename: function (req, file, cb) {
     cb(null, uuidv4() + "-" + Date.now() + path.extname(file.originalname));
